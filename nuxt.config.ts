ed97d9f734d2f6ee77cd01852dcf2f0d51e40c67
Nicklasdegnebolig <nicklasdegnebolig@gmail.com>
2024-07-31 22:07:00 +0200

    added nuxt studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content'],
+  modules: ['@nuxt/content', '@nuxthq/studio'],
   routeRules: {
     '/': { prerender: true }
+  },
+
+  studio: {
+    enabled: true
   }
 })
